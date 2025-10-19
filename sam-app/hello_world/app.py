a391cb7dc31342f8dbdb7eaeeba580be0c24ac13
Baki Tuna Yıldız <bakitunayildiz@gmail.com>
2025-10-19 12:35:17 +0300

    Delete sam-app/hello_world directory

--- sam-app/hello_world/app.py
+++
@@ -1,8 +0,0 @@
-import json
-
-def lambda_handler(event, context):
-    return {
-        "statusCode": 200,
-        "headers": {"content-type": "application/json"},
-        "body": json.dumps({"message": "Hello World from SAM!"})
-    }
